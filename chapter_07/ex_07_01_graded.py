c27b4f9a7c286071c7059a0fa680c38956c197d3
Przemyslaw <przemyslaw.chojnacki@wundermanthompson.com>
2024-09-11 14:18:03 +0200

    ch07 folder

---
+++ chapter_07/ex_07_01_graded.py
@@ -0,0 +1,11 @@
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    quit()
+for word in fh:
+    uword = word.upper()
+    strword = uword.rstrip()
+    print(strword)
